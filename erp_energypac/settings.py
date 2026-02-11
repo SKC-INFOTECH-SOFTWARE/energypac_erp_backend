1acf906b886dfc1e2bc63721ab506f41064d0030
dev-soumyajit <soumyajitkhan6@gmail.com>
2026-02-11 15:27:44 +0530

    feat: Add initial Django project settings, including REST Framework, JWT, CORS, and custom application configurations.

--- erp_energypac/settings.py
+++ erp_energypac/settings.py
@@ -105,7 +105,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
